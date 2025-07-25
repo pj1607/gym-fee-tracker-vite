a89d29105a341d21b7c513f5f93fce90c58b7747
Pranjal Maharaniya <pj3511044@gmail.com>
2025-07-25 14:38:01 +0530

    fix: set cookie sameSite=None and secure=true for cross-origin auth

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -64,7 +64,7 @@ export const registerUser = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'Lax',
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
@@ -126,7 +126,7 @@ export const loginUser = async (req, res) => {
   res.cookie('token', token, {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'Lax', 
+  sameSite: 'None',
   maxAge: 7 * 24 * 60 * 60 * 1000 
 });
 
