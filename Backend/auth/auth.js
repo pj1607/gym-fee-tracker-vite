acc8a0289c0f0ad9666f685f5583117e166883c8
Pranjal Maharaniya <pj3511044@gmail.com>
2025-07-26 17:19:04 +0530

    Fix: updated

--- Backend/auth/auth.js
+++ Backend/auth/auth.js
@@ -27,7 +27,7 @@ export const verifyToken = async (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
 
     if (!token) {
-      return res.status(401).json({ message: 'Unauthorized: No token', success: 'no' });
+      return res.status(401).json({ message: 'Try again, please.', success: 'no' });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
